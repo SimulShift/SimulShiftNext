97ce5e19e4d879c083d829ab81d33bd26fe8185e
SimulShift <simulshiftsocial@gmail.com>
2023-06-03 00:17:04 -0700

    Added environment variable for parent domain to load twitch player

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <main className={styles.main}>
       <iframe
-        src="https://player.twitch.tv/?channel=simulshift&parent=localhost&muted=true"
+        src={`https://player.twitch.tv/?channel=simulshift&parent=${process.env.NEXT_PUBLIC_PARENT_DOMAIN}&muted=true`}
         height="400"
         width="800"
         allowFullScreen></iframe>
