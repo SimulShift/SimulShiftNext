74994840ded5ba83d1e4b7c9c3eb0a31b0eb3e8d
Alexander Pena <thesimulshift@gmail.com>
2023-06-22 18:56:32 -0700

    switched to axios

--- app/api/twitch/admin/tmi/status/route.tsx
+++ app/api/twitch/admin/tmi/status/route.tsx
@@ -1,10 +1,11 @@
 const backendUrl = process.env.EXPRESS_BACKEND_URL
 const twitchAdminEndpoint = `${backendUrl}/twitch/admin`
+import axios from 'axios'
 
 export const GET = async () => {
   try {
     console.log('twichAdminEndpoint: ', twitchAdminEndpoint)
-    return fetch(`${twitchAdminEndpoint}/tmi/status`, {cache: 'no-store'})
+    return axios.get(`${twitchAdminEndpoint}/tmi/status`)
     //return new Response('test', {status: response.status})
   } catch (error) {
     console.error('tmiStatus: ', error)
