b05fc774360ea5327dcb8e47515519ba423ace1e
Alexander Pena <thesimulshift@gmail.com>
2023-06-25 20:50:43 -0700

    removed style import not used

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,9 +1,7 @@
-import styles from './styles.module.css'
-
 const AboutLayout = ({children}: {children: React.ReactNode}) => {
   return (
     <>
-      <main className={styles.main}>{children}</main>
+      <>{children}</>
     </>
   )
 }
