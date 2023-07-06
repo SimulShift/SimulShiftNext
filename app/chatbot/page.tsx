81e21d445a150ffd801f6c027e7d40516fc227d8
Alexander Pena <arpenacoding@gmail.com>
2023-07-06 14:24:35 -0700

    adding backend fetch test

--- app/chatbot/page.tsx
+++ app/chatbot/page.tsx
@@ -8,9 +8,20 @@ import {useSession} from 'next-auth/react'
 import ControlPanel from './ControlPanel'
 import Typography from '@mui/material/Typography'
 import {Container} from '@mui/material'
+import {useEffect} from 'react'
 
 const ChatBotPage = () => {
   const {data: session} = useSession({required: true})
+
+  useEffect(() => {
+    backendFetchTest()
+  }, [])
+
+  const backendFetchTest = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/test`)
+    const data = await res.json()
+    console.log(data)
+  }
 
   return (
     <Container maxWidth="md">
